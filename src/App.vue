3f7e153b9d5c0a33027767b6bf1a66772eb02954
PMishra-98 <1998pallawi@gmail.com>
2024-02-04 08:48:06 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <!-- App.vue -->
+
+  <v-app>
+
+
+    <v-app-bar>
+
+      <v-avatar color="teal" rounded size="56" tile >
+        <v-icon dark>
+          mdi-account-circle
+        </v-icon><br/>
+        ADD
+      </v-avatar>
+    </v-app-bar>
+
+    <!-- Sizes your content based upon application components -->
+    <v-main>
+
+      <!-- Provides the application the proper gutter -->
+      <v-container>
+
+        <!-- If using vue-router -->
+        <router-view></router-view>
+      </v-container>
+    </v-main>
+
+    <v-footer>
+      @Pallawi
+    </v-footer>
+  </v-app>
+</template>
+
+<script script>
+
+
+
+</script>
